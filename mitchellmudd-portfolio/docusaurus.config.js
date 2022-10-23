e7cfbfee3aa30bf7044d84102dd6c6a3a39e36ae
Mitchell Mudd <mitchellmudd@outlook.com>
2022-10-23 19:25:07 -0400

    Adding base URL

--- mitchellmudd-portfolio/docusaurus.config.js
+++ mitchellmudd-portfolio/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Hi, I am Mitchell Mudd!',
   tagline: 'A Web Developer :)',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://mitchellmudd.tech',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
