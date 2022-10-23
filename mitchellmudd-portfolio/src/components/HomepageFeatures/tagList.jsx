f532badc493e9a4865c1caa0508678091f6563e4
Mitchell Mudd <mitchellmudd@outlook.com>
2022-10-23 15:21:18 -0400

    Fixed the tags issue (so says the build preview)

--- mitchellmudd-portfolio/src/components/HomepageFeatures/tagList.jsx
+++ mitchellmudd-portfolio/src/components/HomepageFeatures/tagList.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './styles.module.css'
 export default function TagList({ tags }) {
     return (
         <>
-            <ul className={'tags_node_modules-@docusaurus-theme-classic-lib-theme-TagsListInline-styles-module padding--none margin-left--sm'}>
+            <ul className={styles.tagList}>
 
                 {tags.map((tag, idx) => (
-                    <li key={idx} className={'tag_node_modules-@docusaurus-theme-classic-lib-theme-TagsListInline-styles-module'}>
+                    <li key={idx} className={styles.tagItem}>
 
-                        <a key={idx} className={'tag_node_modules-@docusaurus-theme-classic-lib-theme-Tag-styles-module tagRegular_node_modules-@docusaurus-theme-classic-lib-theme-Tag-styles-module'}>{tag}</a></li>
+                        <a key={idx} className={styles.tagLabel}>{tag}</a></li>
                 ))}
             </ul>
         </>
