8566fbc7263c830f9b8ac20e985923456bbedd54
Mitchell Mudd <mitchellmudd@outlook.com>
2022-10-22 23:14:22 -0400

    Added author profile for me

---
+++ mitchellmudd-portfolio/blog/2021-07-04-first-blog-post.md
@@ -0,0 +1,26 @@
+---
+slug: first-blog-post
+title: You should shadow your dream job
+authors: Mitchell 
+tags: [career]
+---
+
+## Calculator project:
+
+I started my calculator project on a plane bound for Seattle Washington. The funny thing was I worked on it for an hour and then got stuck because I couldn’t get the calculator grid to show up at all and had no internet on the plane to look up the issue. When I landed in Seattle that night I found out that the issue was...
+
+<!--truncate-->
+
+ that I forgot to link my style sheet to my HTML page ***facepalm*****. After that silly mishap, I got to work on all of the functions and made the operator functions… functional. From there, things went smoothly and I was able to add keyboard controls, added exponent notation after the number got too large, and styled it to look like the iPhone calculator. The smallest brain thing I did throughout this project was making the percent button edit the string that was in the input box. This took a lot of lines of code and many a splice method when all I needed to do was divide the current number by 100. In the end, I was incredibly happy with my project and might add more functionality in the future if I’m ever feeling uninspired.
+
+## Job shadow:
+
+A little over 6 months ago I had the pleasure of going on a job shadow with a project manager at my current place of employment, Tommy car wash systems. I went to work learning how to read building plans and researching how I could reach my goal of becoming a project manager. Reading blueprints without being able to apply that skill got boring quickly and to improve my skills in my current role I took a Udemy course on Microsoft Excel. Towards the end of that course, there was a small section on Visual Basic which triggered an interest in programming. Then I bought a python course on Udemy. Since then I’ve completed most of the python course (skipped the specific python related stuff) and opted to learn JavaScript to pursue front-end web development. Just recently I was allowed to go on another job shadow within the company I work at. I shadowed our web developer for half the day then one of our software developers. It was a fantastic day and it gave me a lot of perspective on where I can improve. I realized that I have a lot of room to grow before I can call myself a software developer, but I also found that I**’m closer to securing a front-end web developer job than I had thought**. The job shadow instilled motivation in me to continue learning and striving for career improvement. If you ever have a chance to do a job shadow for a position you hope to reach I advise you to **take the opportunity**.
+
+## Learning:
+
+The only learning that I did that wasn’t related to solving bugs in projects was on object-oriented programming in javascript. I never truly grasped OOP when learning python and when learning about objects it made a lot more sense this time around. I’ll end up learning more about this over the next couple of weeks.
+
+## Lessons Learned:
+
+Learning how to use HTML, CSS, and JS well isn’t easy. **I am having a lot of fun** improving my skills in all three though and I don’t see myself stopping anytime soon. Thank you for reading!
--- mitchellmudd-portfolio/docusaurus.config.js
+++ mitchellmudd-portfolio/docusaurus.config.js
@@ -14,11 +14,6 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: '/img/logo_transparent.png',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -36,15 +31,12 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+         
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -69,9 +61,9 @@ const config = {
             position: 'left',
             label: 'Skills',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/mitchelldirt',
             label: 'GitHub',
             position: 'right',
           },
@@ -81,28 +73,19 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Skills',
-                to: '/docs/Skills/My%20Favorite%20Tech%20Stack',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/mitchelldirt',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/in/mitchellmudd/',
+              },
+              {
+                label: 'Hashnode',
+                href: 'https://hashnode.com/@mitchelldirt',
               },
             ],
           },
@@ -114,8 +97,12 @@ const config = {
                 to: '/blog',
               },
               {
+                label: 'Skills',
+                to: '/docs/Skills/My%20Favorite%20Tech%20Stack',
+              },
+              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/mitchelldirt',
               },
             ],
           },
