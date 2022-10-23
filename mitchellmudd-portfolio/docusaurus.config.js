0ed1ecef1291672ba66b0e39eb288c4d668dc294
Mitchell Mudd <mitchellmudd@outlook.com>
2022-10-22 22:42:21 -0400

    Fixed favicon lol

--- mitchellmudd-portfolio/docusaurus.config.js
+++ mitchellmudd-portfolio/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: './static/img/favicon.png',
+  favicon: '/img/logo_transparent.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
