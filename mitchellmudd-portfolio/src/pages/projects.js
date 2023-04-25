644090ace08c8bccefb499eaca833c367c91d162
mitchelldirt <mitchellmudd@outlook.com>
2023-04-25 18:48:02 -0400

    Added missing aria-label to project nav buttons

--- mitchellmudd-portfolio/src/pages/projects.js
+++ mitchellmudd-portfolio/src/pages/projects.js
@@ -86,7 +86,7 @@ export default function Projects() {
       <Project project={projects[currentProject]} isFeatured={false} />
 
       <div className={styles.projectLinks}>
-        <button onClick={prevProject}>
+        <button aria-label='Previous Project' onClick={prevProject}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={styles.navButton}>
             <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-4.28 9.22a.75.75 0 000 1.06l3 3a.75.75 0 101.06-1.06l-1.72-1.72h5.69a.75.75 0 000-1.5h-5.69l1.72-1.72a.75.75 0 00-1.06-1.06l-3 3z" clipRule="evenodd" />
           </svg>
@@ -100,7 +100,7 @@ export default function Projects() {
         <svg id='project2' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={clsx(currentProject === 2 ? styles.selectedProjectNav : '', styles.navButton)}>
           <circle cx="12" cy="12" r="9.75" />
         </svg>
-        <button onClick={nextProject}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={styles.navButton}>
+        <button aria-label='Next Project' onClick={nextProject}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={styles.navButton}>
           <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" clipRule="evenodd" />
         </svg>
         </button>
